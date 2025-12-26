8b5cf1ca353e648671855578094e9c3fa96b6cbb
someusernu <foh@pan-therra.ru>
2025-12-26 04:17:06 +0300

    implementation for insertion sort

--- src/nr/foh/DatStruct/Main.java
+++ src/nr/foh/DatStruct/Main.java
@@ -1,15 +1,20 @@
 package nr.foh.DatStruct;
+import nr.foh.Sorts.InsertSort;
 
 public class Main {
     public static void main(String[] args) {
-        Dqueue<Integer> s = new Dqueue<>(54);
-        for (int i = 6; i < 163; i += 3) {s.putFirst(i); if (i == 9){s.putFirst(54);}}
-        for (int i : s) {System.out.println(i);}
-        s.putLast(54);
-        System.out.println(s.count(54));
-        while (s.getSize() > 0) {s.takeLast();}
-        System.out.println();
-        System.out.println();
-        for (int i : s) {System.out.println(i);}
+        Integer[] a = new Integer[8];
+        a[0] = 1;
+        a[1] = 4;
+        a[2] = 5;
+        a[3] = 7;
+        a[4] = 13;
+        a[5] = 27;
+        a[6] = 314;
+        a[7] = 412;
+        InsertSort.inssort(a, true);
+        for (int i : a) {
+            System.out.println(i);
+        }
     }
 }
